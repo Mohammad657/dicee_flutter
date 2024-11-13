1b7426643c8440b42c51457a6ca50fa7882b9124
Mohammad657 <Alalaqm215@gmail.com>
2024-11-13 21:59:58 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        elevation: 4,
+        shadowColor: Colors.black,
+        title: const Text("Dice"),
+        backgroundColor: Colors.red,
+      ),
+      body: const MyApp(),
+    ),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image.asset("images/dice$leftDiceNumber.png"),
+                onPressed: () {
+                  randomDice();
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image.asset(
+                  "images/dice$rightDiceNumber.png",
+                ),
+                onPressed: () {
+                  randomDice();
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
